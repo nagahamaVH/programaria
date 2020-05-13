28026a78d8a47359c8c679f839248cf74aa4bcca
Victor <victorh.nagahama@gmail.com>
2020-05-12 23:48:21 -0300

    refactor: 💡 prepare categorical

--- imdb/src/prepare-data-categorical.py
+++ imdb/src/prepare-data-categorical.py
@@ -9,12 +9,15 @@ imdb = raw_imdb.copy()
 
 imdb['aspect_ratio'] = imdb['aspect_ratio'].astype('str')
 
-imdb['categorical_imdb_score'] = pd.cut(imdb['imdb_score'], 
-bins=[0, 4, 6, 8, 10], right=True, labels=False) + 1
+imdb['categorical_imdb_score'] = pd.cut(
+    imdb['imdb_score'], bins=[0, 4, 6, 8, 10], right=True, labels=False) + 1
+
+imdb['categorical_imdb_score'] = imdb['categorical_imdb_score'].astype('str')
 
 # Useless columns
-useless_col = ['movie_imdb_link', 'movie_title', 'imdb_score', 'plot_keywords',
-'actor_2_name', 'actor_3_name']
+useless_col = [
+    'movie_imdb_link', 'movie_title', 'imdb_score', 'plot_keywords', 
+    'actor_2_name', 'actor_3_name']
 
 imdb.drop(useless_col, axis=1, inplace=True)
 
@@ -29,13 +32,12 @@ imdb['return_investment'] = imdb['return_investment'].mask(
     imdb['return_investment'] < 0, 0)
 
 # --- Transforming quantitative variables to log scale
-quantitative = imdb.select_dtypes(include=['float64', 'int64']).drop(
-    ['categorical_imdb_score'], axis=1)
+quantitative = imdb.select_dtypes(include=['float64', 'int64'])
 
 quantitative = quantitative.transform(lambda x: np.log(x + 0.0001))
 
 imdb = imdb.select_dtypes(exclude=['float64', 'int64'])
-imdb = pd.concat([imdb, quantitative], axis=1)
+imdb = imdb.join(quantitative)
 
 # --- Grouping levels of aspect ratio of screen
 for i in range(0, len(imdb)):
@@ -64,15 +66,20 @@ for i in range(0, len(imdb)):
     'Australia'] and str(value) != 'nan':
         imdb.loc[i, 'country'] = 'Other'
 
-# --- Creating dummy varible of genres 
+# --- Creating dummy varible of genres
 genres_list = imdb['genres'].str.split('|').tolist()
 
 unique_genres = set(list(itertools.chain.from_iterable(genres_list)))
 
-genres_dummy = imdb['genres'].str.get_dummies().add_prefix('genre_')
+genres_dummy = imdb['genres'].str.get_dummies()#.add_prefix('genre_')
+
+genre_effect = [
+    'Animation', 'Biography', 'Comedy', 'Documentary', 'Drama', 'History',
+    'Music', 'Musical', 'Mystery', 'War']
 
 imdb.drop(['genres'], axis=1, inplace=True)
-imdb = pd.concat([imdb, genres_dummy], axis=1)
+genres_dummy = genres_dummy[genre_effect]
+imdb = imdb.join(genres_dummy)
 
 # --- Creating dummy varible of top-k directors 
 director_dummy = get_dummies_based_variables('categorical_imdb_score', 
@@ -81,7 +88,7 @@ director_dummy = get_dummies_based_variables('categorical_imdb_score',
 director_dummy = director_dummy.add_prefix('director_')
 
 imdb.drop(['director_name'], axis=1, inplace=True)
-imdb = pd.concat([imdb, director_dummy], axis=1)
+imdb = imdb.join(director_dummy)
 
 # --- Creating dummy varible of top-k actors 
 actor_dummy = get_dummies_based_variables('categorical_imdb_score', 
@@ -90,15 +97,51 @@ actor_dummy = get_dummies_based_variables('categorical_imdb_score',
 actor_dummy = actor_dummy.add_prefix('actor_')
 
 imdb.drop(['actor_1_name'], axis=1, inplace=True)
-imdb = pd.concat([imdb, actor_dummy], axis=1)
+imdb = imdb.join(actor_dummy)
 
-# --- Inpute missing data
+# --- Handle with missing data
 frequency = imdb.isnull().sum().sort_values()
 percentual = imdb.isnull().sum() / imdb.isnull().count()
 
 missing_data = pd.concat([frequency, percentual], axis=1, 
 keys=['frequency', 'percentual']).sort_values('percentual', ascending=False)
 
-imdb.columns
+# --- Creating dummy varible of aspect_ratio
+aspect_dummy = imdb['aspect_ratio'].str.get_dummies().add_prefix('aspect_')
 
-#imdb.to_csv('./imdb/data/categorical_imdb.csv', index=False, sep=';')
+imdb.drop(['aspect_ratio'], axis=1, inplace=True)
+imdb = imdb.join(aspect_dummy)
+
+# --- Creating dummy varible of content_rating
+content_dummy = imdb['content_rating'].str.get_dummies().add_prefix('content_')
+
+imdb.drop(['content_rating'], axis=1, inplace=True)
+imdb = imdb.join(content_dummy)
+
+# --- Creating dummy varible of color
+color_dummy = imdb['color'].str.get_dummies().add_prefix('color_')
+
+imdb.drop(['color'], axis=1, inplace=True)
+imdb = imdb.join(color_dummy)
+
+# --- Creating dummy varible of language
+language_dummy = imdb['language'].str.get_dummies().add_prefix('language_')
+
+imdb.drop(['language'], axis=1, inplace=True)
+imdb = imdb.join(language_dummy)
+
+# --- Creating dummy varible of country
+country_dummy = imdb['country'].str.get_dummies().add_prefix('country_')
+
+imdb.drop(['country'], axis=1, inplace=True)
+imdb = imdb.join(country_dummy)
+
+# --- Quantitative imputation
+missing_data = imdb.isnull().any()
+missing_columns = missing_data.index[missing_data].tolist()
+median_values = imdb[missing_columns].median()
+
+imdb_clean = imdb.copy()
+imdb_clean[missing_columns] = imdb_clean[missing_columns].fillna(median_values)
+
+imdb_clean.to_csv('./imdb/data/categorical_imdb.csv', index=False)
